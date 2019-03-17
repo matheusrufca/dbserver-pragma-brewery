66e2259092ae9a79710292d43afcf364a74e05cb
Matheus Rufca <matheus.rufca@portotech.org>
2019-03-17 17:40:49 -0300

    Added tests for BeerContainerTemperatureService

--- src/app/services/thermometer/beer-container-temperature.service.spec.ts
+++ src/app/services/thermometer/beer-container-temperature.service.spec.ts
@@ -1,12 +1,54 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { BeerContainerTemperatureService } from './beer-container-temperature.service';
+import { Observable } from 'rxjs';
 
 describe('BeerContainerTemperatureService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: BeerContainerTemperatureService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(BeerContainerTemperatureService);
+  });
 
   it('should be created', () => {
-    const service: BeerContainerTemperatureService = TestBed.get(BeerContainerTemperatureService);
     expect(service).toBeTruthy();
+  });
+
+  describe('getCurrentTemperature()', () => {
+    const TEMPERATURE_RANGE = Object.freeze({
+      min: 2,
+      max: 9,
+    });
+    const FETCH_INTERVAL: number = 5000;
+    const TEMPERATURE_INCREMENT: number = 2;
+
+    it('should return an observable', () => {
+      const currentTemperature: Observable<number> = service.getCurrentTemperature();
+      expect(currentTemperature).toBeDefined();
+    });
+
+    it(`should update temperature value every ${FETCH_INTERVAL}`, fakeAsync(() => {
+      let currentTemperature: number, fetchingTimes: number = 0;
+      const currentTemperature$: Observable<number> = service.getCurrentTemperature();
+      const subscription = currentTemperature$.subscribe((temperature) => {
+        currentTemperature = temperature;
+        fetchingTimes++;
+      });
+
+      tick(FETCH_INTERVAL);
+      expect(fetchingTimes).toEqual(1);
+      expect(currentTemperature).toBeGreaterThanOrEqual(TEMPERATURE_RANGE.min - TEMPERATURE_INCREMENT);
+      expect(currentTemperature).toBeLessThanOrEqual(TEMPERATURE_RANGE.max + TEMPERATURE_INCREMENT);
+
+      tick(FETCH_INTERVAL);
+      expect(currentTemperature).toBeGreaterThanOrEqual(TEMPERATURE_RANGE.min - TEMPERATURE_INCREMENT);
+      expect(currentTemperature).toBeLessThanOrEqual(TEMPERATURE_RANGE.max + TEMPERATURE_INCREMENT);
+
+      expect(fetchingTimes).toEqual(2);
+      expect(currentTemperature).toBeGreaterThanOrEqual(TEMPERATURE_RANGE.min - TEMPERATURE_INCREMENT);
+      expect(currentTemperature).toBeLessThanOrEqual(TEMPERATURE_RANGE.max + TEMPERATURE_INCREMENT);
+
+      subscription.unsubscribe();
+    }))
   });
 });
--- src/app/services/thermometer/beer-container-temperature.service.ts
+++ src/app/services/thermometer/beer-container-temperature.service.ts
@@ -2,42 +2,47 @@ import { Injectable } from '@angular/core';
 import { Random } from 'random-js';
 import { interval, Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
+import { TemperatureRange } from 'src/app/models/temperature-range';
 
-const TEMPERATURE_RANGE = Object.freeze({
-  min: 0,
-  max: 10,
+const TEMPERATURE_RANGE: TemperatureRange = Object.freeze({
+  min: 2,
+  max: 9,
 });
+const FETCH_INTERVAL: number = 5000;
+const TEMPERATURE_INCREMENT: number = 2;
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerContainerTemperatureService {
   private readonly random: Random;
-  private readonly $current: Observable<number>;
+  private readonly currentTemperature$: Observable<number>;
   private currentTemperature: number;
 
   constructor() {
     this.random = new Random();
-    this.currentTemperature = this.fetchCurrentTemperature();
-
-    this.$current = interval(5000)
+    this.currentTemperature = this.random.real(TEMPERATURE_RANGE.min, TEMPERATURE_RANGE.max);
+    this.currentTemperature$ = interval(FETCH_INTERVAL)
       .pipe(
-        tap(this.setCurrentTemperature.bind(this)),
-        map((value: any) => this.fetchCurrentTemperature(this.currentTemperature))
+        map((value: any) => this.fetchCurrentTemperature(this.currentTemperature)),
+        tap(this.setCurrentTemperature.bind(this))
       );
   }
 
   getCurrentTemperature(): Observable<number> {
-    return this.$current;
+    return this.currentTemperature$;
   }
 
-  private fetchCurrentTemperature(currentTemperature: number = 15): number {
-    // const minTemperature = TEMPERATURE_RANGE.min < currentTemperature ? (currentTemperature - 30) : TEMPERATURE_RANGE.min;
-    // const maxTemperature = TEMPERATURE_RANGE.max > currentTemperature ? (currentTemperature + 30) : TEMPERATURE_RANGE.max;
-    return this.random.real(TEMPERATURE_RANGE.min, TEMPERATURE_RANGE.max);
+  private fetchCurrentTemperature(temperature: number): number {
+    return this.random.real(temperature - TEMPERATURE_INCREMENT, temperature + TEMPERATURE_INCREMENT);
   }
 
-  private setCurrentTemperature(current: number): void {
-    this.currentTemperature = current;
+  private setCurrentTemperature(temperature: number): void {
+    if (temperature > TEMPERATURE_RANGE.max)
+      temperature = TEMPERATURE_RANGE.max;
+    else if (temperature < TEMPERATURE_RANGE.min)
+      temperature = TEMPERATURE_RANGE.min;
+
+    this.currentTemperature = temperature;
   }
 }
