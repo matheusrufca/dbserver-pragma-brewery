edac21e522abe2334bec7b20d8fe5f0265889b8d
Matheus Rufca <matheusrufca@gmail.com>
2019-03-18 08:03:09 -0300

    Fix lint warnings

--- src/app/services/beer/beer-container.service.spec.ts
+++ src/app/services/beer/beer-container.service.spec.ts
@@ -4,37 +4,16 @@ import { getTestBed, TestBed } from '@angular/core/testing';
 import { BeerContainerService } from './beer-container.service';
 
 const FAKE_BEER_CONTAINERS = Object.freeze([{
-  "id": 1,
-  "type": "Pilsner",
-  "min": 4,
-  "max": 6
+  id: 1,
+  type: 'Pilsner',
+  min: 4,
+  max: 6
 }, {
-  "id": 2,
-  "type": "IPA",
-  "min": 5,
-  "max": 6
-}, {
-  "id": 3,
-  "type": "Lager",
-  "min": 4,
-  "max": 7
-}, {
-  "id": 4,
-  "type": "Stout",
-  "min": 6,
-  "max": 8
-}, {
-  "id": 5,
-  "type": "Wheat Beer",
-  "min": 3,
-  "max": 5
-}, {
-  "id": 6,
-  "type": "Pale Ale",
-  "min": 4,
-  "max": 6
+  id: 2,
+  type: 'IPA',
+  min: 5,
+  max: 6
 }]);
-
 
 describe('BeerContainerService', () => {
   let injector: TestBed;
